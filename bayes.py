387e21e1799ef26c2f2da402b9bb034ec8a8de15
Blunce <756892169@qq.com>
2015-04-16 16:59:40 +0800

    add a function called loadDataSet

--- bayes.py
+++ bayes.py
@@ -4,3 +4,12 @@ Created on 2015Äê4ÔÂ16ÈÕ
 
 @author: Blunce
 '''
+def loadDataSet():
+    postingList=[['my', 'dog', 'has', 'flea', 'problems', 'help', 'please'],
+                 ['maybe', 'not', 'take', 'him', 'to', 'dog', 'park', 'stupid'],
+                 ['my', 'dalmation', 'is', 'so', 'cute', 'I', 'love', 'him'],
+                 ['stop', 'posting', 'stupid', 'worthless', 'garbage'],
+                 ['mr', 'licks', 'ate', 'my', 'steak', 'how', 'to', 'stop', 'him'],
+                 ['quit', 'buying', 'worthless', 'dog', 'food', 'stupid']]
+    classVec = [0,1,0,1,0,1]    #1 is abusive, 0 not
+    return postingList,classVec
